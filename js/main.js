b04ba327561235af792ddf948f6f82a9292cf93e
Andres Andreone <Andres Andreone>
2017-02-17 00:53:57 -0300

    version 1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    console.log('its working !');
+
+    $("#menu").load("html/menu.html");
+    $("#logo").load("html/logo.html");
+    $("#cuerpo").load("html/cuerpo.html");
+    $("#prefooter").load("html/prefooter.html");
+    $("#footer").load("html/footer.html");
+
+
+
+})
